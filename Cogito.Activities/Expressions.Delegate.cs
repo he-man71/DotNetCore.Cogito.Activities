7569bfc5a9fc95fac79351601e8186b2bb659220
Jerome Haltom <wasabi@cogito.cx>
2020-02-07 10:16:39 -0600

    Initial import.

---
+++ Cogito.Activities/Expressions.Delegate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Activities;
+
+namespace Cogito.Activities
+{
+    public static partial class Expressions
+    {
+
+        public static ActivityAction Delegate(Func<Activity> create)
+        {
+            if (create == null)
+                throw new ArgumentNullException(nameof(create));
+
+            return new ActivityAction()
+            {
+                Handler = create(),
+            };
+        }
+
+        public static ActivityAction<TArg> Delegate<TArg>(Func<DelegateInArgument<TArg>, Activity> create)
+        {
+            if (create == null)
+                throw new ArgumentNullException(nameof(create));
+
+            var arg = new DelegateInArgument<TArg>();
+
+            return new ActivityAction<TArg>()
+            {
+                Argument = arg,
+                Handler = create(arg),
+            };
+        }
+
+    }
+
+}
